2b2d48293f19aeb56ac1037a27f6294cca4adde0
Josh Robar <jerobar@pm.me>
2022-09-03 10:28:41 -0300

    Adding fn selectors

--- YULRC1155.sol
+++ YULRC1155.sol
@@ -1,40 +1,62 @@
 object "YULRC1155" {
 
-    // 'constructor'
     code {
         // Deploy the contract
         datacopy(0, dataoffset("runtime"), datasize("runtime"))
         return(0, datasize("runtime"))
     }
+
     object "runtime" {
         code {
             // Protection against sending ether
             // ?
 
-            // Dispatcher
+            /**
+             * @dev Dispatch to relevant function based on (calldata) function 
+             * selector.
+             */
             switch selector()
-            case 0x00 /* "balanceOf(address,uint256)" */ {}
-            case 0x00 /* "balanceOfBatch(address[] calldata,uint256[] calldata)" */ {}
-            case 0x00 /* "setApprovalForAll(address,bool)" */ {}
-            case 0x00 /* "isApprovedForAll(address,address)" */ {}
-            case 0x00 /* "safeTransferFrom(address,address,uint256,uint256,bytes calldata)" */ {}
-            case 0x00 /* "safeBatchTransferFrom(address,address,uint256[] calldata,uint256[] calldata,bytes calldata)" */ {}
-            
+            // balanceOf(address,uint256)
+            case 0x00fdd58e {
+                returnUint(balanceOf(decodeAsAddress(0), 1234))
+            }
+            // balanceOfBatch(address[],uint256[])
+            case 0x4e1273f4 {}
+            // setApprovalForAll(address,bool)
+            case 0xa22cb465 {}
+            // isApprovedForAll(address,address)
+            case 0xe985e9c5 {}
+            // safeTransferFrom(address,address,uint256,uint256,bytes)
+            case 0xf242432a {}
+            // safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)
+            case 0x2eb2c2d6 {}
             default {
                 revert(0, 0)
             }
 
-            // function balanceOf() {}
-            // function balanceOfBatch() {}
-            // function setApprovalForAll() {}
-            // function isApprovedForAll() {}
-            // function safeTransferFrom() {}
-            // function safeBatchTransferFrom() {}
+            function balanceOf(account, id) {}
+            function balanceOfBatch(accounts, ids) {}
+            function setApprovalForAll(operator, approved) {}
+            function isApprovedForAll(account, operator) {}
+            function safeTransferFrom(from, to, id, amount, data) {}
+            function safeBatchTransferFrom(from, to, ids, amounts, data) {}
 
-            // calldata decoding functions
+            // Calldata decoding functions
             function selector() -> s {
+                // Question: What does `div` do here?
                 s := div(calldataload(0), 0x100000000000000000000000000000000000000000000000000000000)
             }
+
+            // Calldata encoding functions
+            function returnUint(v) {
+                mstore(0, v)
+                return(0, 0x20)
+            }
+            function returnTrue() {
+                returnUint(1)
+            }
+
+            // Utility functions
         }
     }
 }
