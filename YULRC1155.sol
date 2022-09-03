679c96de848e60a8792249468057de9b1fd62c90
Josh Robar <jerobar@pm.me>
2022-09-03 18:40:14 -0300

    notes

--- YULRC1155.sol
+++ YULRC1155.sol
@@ -1,5 +1,4 @@
 /**
-
 NOTES
 
 Calldata example:
@@ -24,24 +23,130 @@ Calldata with array args (address[], uint256[]):
 000000000000000000000000000000000000000000000000000000000000002a 6
 000000000000000000000000000000000000000000000000000000000000002a 7
 
+uint256 x
+sload(x.slot), sstore(slot, value)
+.slot - actual memory location
+.offset - within packed slot (bytes to the left)
+  - bitshifting and masking
+  - e.g if offset 28, shift slot to right shr by 28 bytes
+  E uint8
+  let vaue := sload(E.slot)
+  let shifted := shr(mul(E.offset, 8), value)
+  e := and(0xffff, shifted) // notes leading 0's inplied before fs
+
+*write to packed slot fn eg at 25:24
+
+fixed arrays stored in slots like other vars:
+[1,2,3]
+sload(add(fixedArray.slot, index)) // index 0 ->1
+
+dynamic arrays store their length in their first storage slot
+[1,2,3]
+let dynamicArrayLength := sload(dynamicArray.slot) // 3
+its items are not stored sequentially but rather are stored starting at the slot:
+location = keccak256(abi.encode(dynamicArray.slot)) // index 0
+- then: plus 1, 2, 3, etc. ret := sload(add(location, index))
+
+mappings behave quite similar
+- concatenates key with the storage slot to get the storage location:
+  location = keccak256(abi.encode(key, uint256(slot)))
+  sload(location)
+
+nested mapping are just hashes of hashes
+  nestedMapping[2][4] = 7
+  keccak256(abi.encode(uint256(4), keccak256(abi.encode(uint256(2), uint256(slot))))) // 7
+
+  34:15 mapping onto dynamic array e.g
+
+memory is equivelant to the heap in other langs, laid out in 32 byte sequences addressable by byte
+- distinct from storage above!!
+
+mstore(p, v) stores value v in slot p
+mload(p) retrives 32 bytes from slot p [p...0x20]
+mstore8(p, v) like mstore but for one byte
+msize() largest accessed memory index in that tx
+
+mstore(0x00, 0xff..ff) // 32 bytes of 1's
+0x00 - 0x19 = ff, 0x20 = 00
+// note that mstore0x01 only shifts us forward 1 byte not a full 32 bytes like storage
+// so it writes 32 bytes starting at the second slot
+
+  ) = not inclusive
+- solidity allocates slots 0x00-0x20), 0x20-0x40) for "scratch space"
+- reserves slot 0x40-0x60) as the "free memory pointer"
+- keeps slot 0x60-0x80) open
+- action begins in slot 0x80
+
+solidity uses memory for:
+- abi.encode or abi.encodePacked
+- structs and arrays
+
+in yul to access a dynamic array you have to add 0x20 to skip the length
+
+function args(uint256[] memory arr)
+  location := arr
+  len := mload(arr)
+  valueAtIndex0 = mload(add(arr, 0x20)
+  valueAtIndex1 = mload(add(arr, 0x40)
+
+*evm memory does not pack data types like storage
+
+
+function hash() (53:43) (> 64 bytes = can't just use scratch space)
+
+let freeMemoryPointer := mload(0x40)
+
+mstore(freeMemoryPointer, 1)
+mstore(add(freeMemoryPointer, 0x20), 2)
+mstore(add(freeMemoryPointer, 0x40), 3)
+
+mstore(0x40, add(freeMemoryPointer, 0x60))
+
+mstore(0x00, keccak256(freeMemoryPointer, 0x60))
+return(0x00, 0x20)
+
+
+
+*can deploy a normal sol contract to call this one for testing
+
+
+datacopy(0x00, dataoffset("Foo"), datasize("message")
+data "Foo" "Bar"
+
+*note the cheat that an account is already the hash of a private key so it is random 
+and will not collide (accountToStorageOffset)
+
  */
 
 object "YULRC1155" {
 
     code {
+        // declare state variables here?
+        // sstore(0, caller()) // store msg.sender in slot 0
+        // function owner() return sload(0)
+
         // Deploy the contract
         datacopy(0, dataoffset("runtime"), datasize("runtime"))
         return(0, datasize("runtime"))
     }
 
     object "runtime" {
+
         code {
-            // Protection against sending ether
-            // ?
+            /**
+             * Storage layout
+             */
+            // Mapping uint256 token ID => (address account => uint256 balance)
+            function balancesSlot() -> slot { slot := 0 }
+
+            // Mapping address account => (address operator => bool approved)
+            function operatorApprovalsSlot() -> slot { slot := 1 }
+
+            // accountToStorageOffset ?
 
             /**
              * @dev Dispatch to relevant function based on (calldata) function 
-             * selector.
+             * selector (the first 4 bytes of keccak256(functionSignature)).
              */
             switch getFunctionSelector()
             // balanceOf(address,uint256)
@@ -89,22 +194,37 @@ object "YULRC1155" {
              * Calldata decoding functions
              */
             function getFunctionSelector() -> functionSelector {
-                // Question: What does `div` do here?
+                // `div` shifts right by 224 bits leaving the first 4 bytes
                 functionSelector := div(calldataload(0), 0x100000000000000000000000000000000000000000000000000000000)
+                // Note: shifting would be preferrable as it costs less gas
             }
             function decodeAsAddress(offset) -> value {
                 value := decodeAsUint(offset)
+                revertIfNotValidAddress(value)
             }
             // function decodeAsAddressArray(offset) -> value {}
             // function decodeAsUintArray(offset) -> value {}
             function decodeAsArray(offset) -> value {
-                // 
                 let bitOffsetOfArrayPosition := add(4, mul(offset, 0x20))
                 let bitOffsetOfArray := calldataload(bitOffsetOfArrayPosition)
+                let offsetOfArray := div(bitOffsetOfArray, 0x20)
                 let arrayLength := calldataload(bitOffsetOfArray)
 
-                // Starting at bitOffsetOfArray + 1, loop to bitOffsetOfArray + arrayLength
-                // if arrayLength is zero, return an empty array
+                if iszero(arrayLength) {
+                    // Return empty array
+                    mstore(0x00, 0)
+                    return(0x00, 0x20)
+                }
+
+                // Starting at offsetOfArray + 1, loop to offsetOfArry + arrayLength
+                for { let i := add(offsetOfArray, 1) } lt(i, add(offsetOfArray, arrayLength)) { i := add(i, 1) }
+                {
+                    // 
+                }
+
+                // mstore(0x00, data1)
+                // mstore(0x20, data2)
+                // return (0x00, 0x40)
 
                 // revertIfPositionNotInCalldata(position)
             }
@@ -121,7 +241,7 @@ object "YULRC1155" {
                 let position := add(4, mul(offset, 0x20))
                 revertIfPositionNotInCalldata(position)
                 let valueAtPosition := calldataload(position)
-                revertIfValueNotBool(valueAtPosition)
+                revertIfNotBool(valueAtPosition)
                 value := valueAtPosition
             }
             // function decodeAsBytes(offset) {}
@@ -130,13 +250,13 @@ object "YULRC1155" {
              * Calldata encoding functions
              */
             function returnUint(value) {
-                // Save word `value` to memory at offset 0
+                // Save word `value` to memory at slot 0
                 mstore(0, value)
-                // Return word ('0x20' or 32 bits) from memory offset 0
+                // Return word ('0x20' or 32 bits) from memory slot 0
                 return(0, 0x20)
             }
             function returnBool(value) {
-                revertIfValueNotBool(value)
+                revertIfNotBool(value)
                 mstore(0, value)
                 return(0, 0x20)
             }
@@ -149,7 +269,16 @@ object "YULRC1155" {
                     revert(0, 0)
                 }
             }
-            function revertIfValueNotBool(value) {
+
+            // if iszero(eq(a, b)) revert pattern
+
+            function revertIfNotValidAddress(value) {
+                // Verify this really is an address (and not the zero address)
+                if iszero(iszero(and(value, not(0xffffffffffffffffffffffffffffffffffffffff)))) {
+                    revert(0, 0)
+                }
+            }
+            function revertIfNotBool(value) {
                 let isBool := 0
 
                 if eq(value, 0x0000000000000000000000000000000000000000000000000000000000000000) {
